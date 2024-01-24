36a989c1a39d94fa5c2951714af1abb88a47c594
MuradMT <mammadzade03@gmail.com>
2024-01-24 16:03:44 +0400

    Initial entities added to domain from template.

---
+++ Core/CarBook.Domain/Entities/Banner.cs
@@ -0,0 +1,9 @@
+﻿namespace CarBook.Domain.Entities;
+public class Banner
+{
+    public int BannerID { get; set; }
+    public string Tittle { get; set; }
+    public string Description { get; set; }
+    public string VideoUrl { get; set; }
+    public string VideoDescription { get; set; }
+}
