45322490f467c2cb5b2f8926d999a7549ab74f55
MuradMT <mammadzade03@gmail.com>
2024-03-20 21:32:27 +0400

    Done all CRUD operations.

--- Infrastructure/CarBook.Persistence/Globals.cs
+++ Infrastructure/CarBook.Persistence/Globals.cs
@@ -1,3 +1,5 @@
 ﻿global using Microsoft.EntityFrameworkCore;
 global using CarBook.Helper.Constants;
 global using CarBook.Domain.Entities;
+global using CarBook.Application.Interfaces;
+global using CarBook.Persistence.Contexts;
---
+++ Infrastructure/CarBook.Persistence/Repositories/Repository.cs
@@ -0,0 +1,34 @@
+﻿
+namespace CarBook.Persistence.Repositories;
+public class Repository<T>(CarBookContext _context) : IRepository<T> where T : class
+{
+
+    public async Task<List<T>> GetAllAsync()
+    {
+        return await _context.Set<T>().ToListAsync();
+    }
+
+    public async Task<T> GetByIdAsync(int id)
+    {
+        return await _context.Set<T>().FindAsync(id);
+    }
+    public async Task CreateAsync(T entity)
+    {
+        await _context.Set<T>().AddAsync(entity);
+        await SaveAsync();
+    }
+    public async Task UpdateAsync(T entity)
+    {
+        _context.Set<T>().Update(entity);
+        await SaveAsync();
+    }
+    public async Task RemoveAsync(T entity)
+    {
+        _context.Set<T>().Remove(entity);
+        await SaveAsync();
+    }
+    async Task SaveAsync()
+    {
+        await _context.SaveChangesAsync();
+    }
+}
